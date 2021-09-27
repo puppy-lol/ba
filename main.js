c520e0fc7e14d2d77540d81eea32efe70e8f129a
puppy-lol <77181404+puppy-lol@users.noreply.github.com>
2021-09-27 15:31:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+song = "";
+song2 = "";
+leftWristX = "";
+leftWristY = "";
+rightWristX = "";
+rightWristY = "";
+
+function preload() {
+    song = loadSound("The Duck Song.mp3");
+    song2 = loadSound("The Duck Song 2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + leftWristX + "rightWristY = " + rightWristY);
+    }
+}
